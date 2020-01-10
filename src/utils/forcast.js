c85953b5bf83525c38f7fbc5e4a6f81900674ba9
vivek-shivam <vivekshivam007@gmail.com>
2020-01-10 15:21:04 +0530

    Added new information for forecast data

--- src/utils/forcast.js
+++ src/utils/forcast.js
@@ -10,7 +10,7 @@ const forecast = (latitude,longitude,callback)=>{
             callback('Unable to find location!!!',undefined)
         }
         else{
-            callback(undefined,body.daily.data[0].summary+" It is currently "+ body.currently.temperature +" degrees out. There is a "+body.currently.precipProbability+"% chance of rain.")
+            callback(undefined,body.daily.data[0].summary+" It is currently "+ body.currently.temperature +" degrees out. The high today is "+ body.daily.data[0].temperatureHigh +" and a low of "+ body.daily.data[0].temperatureLow +". There is a "+body.currently.precipProbability+"% chance of rain.")
         }
     })
 }
